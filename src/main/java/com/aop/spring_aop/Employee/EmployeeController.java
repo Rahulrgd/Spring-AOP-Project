e0a14959e18d6ff20f296c628003775a10b964cc
RahulGupta <rahulrgd21@gmail.com>
2024-06-25 16:07:24 +0530

    Implemented methods in controller class

--- src/main/java/com/aop/spring_aop/Employee/EmployeeController.java
+++ src/main/java/com/aop/spring_aop/Employee/EmployeeController.java
@@ -1,5 +1,42 @@
 package com.aop.spring_aop.Employee;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
 public class EmployeeController {
+
+    // Create Employee
+    @PostMapping("/create-employee")
+    public String createEmployee(@RequestBody Employee employee) {
+        return EmployeeServices.createEmployee(employee);
+    }
     
+    // Get All Employee
+    @GetMapping("/all-employee")
+    public void getAllEmployee() {
+        return EmployeeServices.getAllEmployee();
+    }
+    
+    // Get Employee with ID
+    @GetMapping("/employee-with-id")
+    public void getEmployeeWithID(@RequestParam Integer id) {
+        return EmployeeServices.getEmployeeWithID(id);
+    }
+    
+    // Delete Employee with ID
+    @DeleteMapping("/delete-employee-with-id")
+    public void deleteEmployeeWithId(@RequestParam Integer id){
+        return EmployeeServices.deleteEmployeeWithId(id);
+    }
 }
